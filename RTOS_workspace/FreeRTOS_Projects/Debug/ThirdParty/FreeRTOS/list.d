4390e75a7bfb643afc51017556614981cce585b7
Ethan <ethangbraun@gmail.com>
2022-08-23 21:08:46 -0500

    Configured source code to work with Cube IDE

---
+++ RTOS_workspace/FreeRTOS_Projects/Debug/ThirdParty/FreeRTOS/list.d
@@ -0,0 +1,17 @@
+ThirdParty/FreeRTOS/list.o: ../ThirdParty/FreeRTOS/list.c \
+ /home/linkachu/Desktop/freeRTOSWorkspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ /home/linkachu/Desktop/freeRTOSWorkspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ /home/linkachu/Desktop/freeRTOSWorkspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/projdefs.h \
+ /home/linkachu/Desktop/freeRTOSWorkspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/portable.h \
+ /home/linkachu/Desktop/freeRTOSWorkspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ /home/linkachu/Desktop/freeRTOSWorkspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ /home/linkachu/Desktop/freeRTOSWorkspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ /home/linkachu/Desktop/freeRTOSWorkspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/list.h
+/home/linkachu/Desktop/freeRTOSWorkspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+/home/linkachu/Desktop/freeRTOSWorkspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+/home/linkachu/Desktop/freeRTOSWorkspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/projdefs.h:
+/home/linkachu/Desktop/freeRTOSWorkspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/portable.h:
+/home/linkachu/Desktop/freeRTOSWorkspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+/home/linkachu/Desktop/freeRTOSWorkspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+/home/linkachu/Desktop/freeRTOSWorkspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+/home/linkachu/Desktop/freeRTOSWorkspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/list.h:
